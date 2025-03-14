2c50dc5bb8e4e26117f21ed62d2df2ffb3e0a7f2
DaltonIA <drdalton.coach@gmail.com>
2025-03-14 15:52:24 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import OraculoIA360 from "./components/OraculoIA360";
+
+function App() {
+  return (
+    <div className="App">
+      <OraculoIA360 />
+    </div>
+  );
+}
+
+export default App;
